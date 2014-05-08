1c88689e91d8c4008fdde516b8af92b897aefbb8
Kazusa Okuda <kazusa@klamath.jp>
2014-05-08 11:58:25 +0900

    add psw.c

---
+++ psw.c
@@ -0,0 +1,22 @@
+/*
+ *
+ *  PIC16F1 EEPROM Library  Version 0.1
+ *  (C) 2014 Kazusa Okuda (a.k.a. klaNath)
+ *
+ *  This software is freely distributable under the terms of an MIT-style license.
+ *  For details, see the https://github.com/klaNath/programable_switcher.git/LICENSE
+ *
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
